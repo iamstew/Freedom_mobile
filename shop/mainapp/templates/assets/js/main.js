8dfe61297d36a3951426d581d0609206a4b938cc
Alexandr Kazeev <kakylya_ija@bk.ru>
2022-11-07 02:29:07 +0300

    adaptive and complete auth form

--- shop/mainapp/templates/assets/js/main.js
+++ shop/mainapp/templates/assets/js/main.js
@@ -1,15 +1,25 @@
 function toggleForm(event){
-	let logIn = document.querySelector(".log-in")
-	let signUp = document.querySelector(".sign-up")
-	let list = event.target.closest(".toggler").parentNode.classList
+	let logIn = document.querySelector(".log-in"),
+		signUp = document.querySelector(".sign-up"),
+		logToggler = document.querySelector(".log-in__toggle"),
+		signToggler = document.querySelector(".sign-up__toggle"),
+		thumbStyle = document.querySelector(".active-thumb").style,
+		data = event.target.closest(".toggler").dataset.button
 
-	if (!list.contains("active")){
-		if (list === logIn.classList) {
-			list.toggle("active")
-			signUp.classList.remove("active")
-		}else if (list === signUp.classList){
-			list.toggle("active")
-			logIn.classList.remove("active")
-		}
+	if (data === "log-in"){
+		signUp.classList.remove("active")
+		logIn.classList.add("active")
+		logToggler.classList.add("active")
+		signToggler.classList.remove("active")
+		thumbStyle.transform = "translateX(0)"
+		// document.querySelector(".auth-form__section").style.transform = "rotateY(0)"
+
+	}else if (data === "sign-up"){
+		signUp.classList.add("active")
+		logIn.classList.remove("active")
+		logToggler.classList.remove("active")
+		signToggler.classList.add("active")
+		thumbStyle.transform = "translateX(100%)"
+		// document.querySelector(".auth-form__section").style.transform = "rotateY(90deg)"
 	}
 }
