9e12ba0881d5ebc61c7df38c513241337c44ad4b
Alexandr Kazeev <kakylya_ija@bk.ru>
2022-11-10 18:18:21 +0300

    hover effect in header

--- shop/mainapp/templates/assets/js/main.js
+++ shop/mainapp/templates/assets/js/main.js
@@ -1,25 +1,16 @@
 function toggleForm(event){
 	let logIn = document.querySelector(".log-in"),
 		signUp = document.querySelector(".sign-up"),
-		logToggler = document.querySelector(".log-in__toggle"),
-		signToggler = document.querySelector(".sign-up__toggle"),
-		thumbStyle = document.querySelector(".active-thumb").style,
+		thumb = document.querySelector(".active-thumb"),
 		data = event.target.closest(".toggler").dataset.button
 
 	if (data === "log-in"){
 		signUp.classList.remove("active")
 		logIn.classList.add("active")
-		logToggler.classList.add("active")
-		signToggler.classList.remove("active")
-		thumbStyle.transform = "translateX(0)"
-		// document.querySelector(".auth-form__section").style.transform = "rotateY(0)"
-
+		thumb.classList.toggle("right")
 	}else if (data === "sign-up"){
 		signUp.classList.add("active")
 		logIn.classList.remove("active")
-		logToggler.classList.remove("active")
-		signToggler.classList.add("active")
-		thumbStyle.transform = "translateX(100%)"
-		// document.querySelector(".auth-form__section").style.transform = "rotateY(90deg)"
+		thumb.classList.toggle("right")
 	}
 }
