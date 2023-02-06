dc187b51dd6b0ad8dbf10a222fdf8e9bebc075e6
Kazeev Aleksandr <kakylya_ija@bk.ru>
2023-02-06 10:33:08 +0300

    Чистка ветки для слияния

--- shop/account/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    # post views
-    url(r'^login/$', views.user_login, name='login'),
-]
--- shop/mainapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def auth(request):
-    return render(request, 'auth.html')
-
-def home(request):
-    return render(request, 'home.html')
